82dd9f66c9b8f69dde2d51eba643b29763aea1bc
yingying <chen.yingying.01988@gmail.com>
2018-10-03 14:37:26 +0100

    test passed vehicle getter functions

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,23 +1,41 @@
 import Component.Engine;
 import Component.Tyre;
 
+import java.util.ArrayList;
+
 public class Vehicle {
     private Engine engine;
-    private Tyre tyre;
+    private ArrayList<Tyre> tyres;
     private Type type;
+    private int price;
+    private String color;
 
-    public Vehicle(Engine engine, Tyre tyre, Type type) {
+    public Vehicle(Engine engine,Type type, ArrayList<Tyre> tyres, int price, String color) {
         this.engine = engine;
-        this.tyre = tyre;
+        this.tyres = new ArrayList<>();
         this.type = type;
+        this.price = price;
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
     }
 
     public Engine getEngine() {
         return engine;
     }
 
-    public Tyre getTyre() {
-        return tyre;
+    public void addTyre(Tyre tyre) {
+        tyres.add(tyre);
+    }
+
+    public int getTyreNumber() {
+        return tyres.size();
     }
 
     public Type getType() {
--- src/test/java/VehicleTest.java
+++ src/test/java/VehicleTest.java
@@ -4,23 +4,57 @@ import com.sun.deploy.security.ValidationState;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class VehicleTest {
 
     Vehicle vehicle;
     Engine engine1;
-    Tyre tyre;
+    Tyre tyre1, tyre2, tyre3, tyre4;
+    ArrayList <Tyre> tyres;
 
     @Before
     public void setUp(){
-        this.engine1 = new Engine(100);
-        this.tyre = new Tyre();
-        this.vehicle = new Vehicle(engine1, tyre,Type.CAR);
+        engine1 = new Engine(100);
+        tyre1 = new Tyre();
+        tyre2 = new Tyre();
+        tyre3 = new Tyre();
+        tyre4 = new Tyre();
+        tyres = new ArrayList<>();
+        vehicle = new Vehicle(engine1, Type.CAR, tyres, 500, "Black");
+        vehicle.addTyre(tyre1);
+        vehicle.addTyre(tyre2);
+        vehicle.addTyre(tyre3);
+        vehicle.addTyre(tyre4);
+    }
+
+    @Test
+    public void canGetVehiclePrice() {
+        assertEquals(500, vehicle.getPrice());
+    }
+
+    @Test
+    public void canGetVehicleColo() {
+        assertEquals("Black", vehicle.getColor());
+    }
+
+    @Test
+    public void canGetVehicleType() {
+        assertEquals(Type.CAR, vehicle.getType());
     }
 
     @Test
     public void canGetEngineHorsePower() {
         assertEquals(100, vehicle.getEngine().getHorsePower());
     }
+
+    @Test
+    public void canGetTyreNumber() {
+        assertEquals(4, vehicle.getTyreNumber());
+    }
+
+
+
 }
